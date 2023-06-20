19b348d9d2d5c84291bce520d3543025768d4e89
Semyon <sema.perstnev.01@mail.ru>
2023-06-20 15:55:48 +0300

    first commit

---
+++ src/main/java/webapp/my/webappdemo/WebAppDemoApplication.java
@@ -0,0 +1,13 @@
+package webapp.my.webappdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebAppDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/webapp/my/webappdemo/WebAppDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package webapp.my.webappdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAppDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
